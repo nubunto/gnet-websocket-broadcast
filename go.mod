b45730aacb8160fe3beb84c66c8591b7dc281876
Bruno Silva <bl.panuto@gmail.com>
2022-06-07 10:30:21 -0300

    initial commit

---
+++ go.mod
@@ -0,0 +1,19 @@
+module github.com/nubunto/gnet-websocket
+
+go 1.17
+
+require (
+	github.com/gobwas/ws v1.1.0
+	github.com/panjf2000/gnet/v2 v2.0.3
+)
+
+require (
+	github.com/gobwas/httphead v0.1.0 // indirect
+	github.com/gobwas/pool v0.2.1 // indirect
+	github.com/panjf2000/ants/v2 v2.4.8 // indirect
+	go.uber.org/atomic v1.9.0 // indirect
+	go.uber.org/multierr v1.8.0 // indirect
+	go.uber.org/zap v1.21.0 // indirect
+	golang.org/x/sys v0.0.0-20220224120231-95c6836cb0e7 // indirect
+	gopkg.in/natefinch/lumberjack.v2 v2.0.0 // indirect
+)
---
+++ websocket.go
@@ -0,0 +1,165 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"log"
+	"sync/atomic"
+	"time"
+
+	"github.com/gobwas/ws"
+	"github.com/gobwas/ws/wsutil"
+	"github.com/panjf2000/gnet/v2"
+	"github.com/panjf2000/gnet/v2/pkg/logging"
+	"github.com/panjf2000/gnet/v2/pkg/pool/goroutine"
+)
+
+type wsServer struct {
+	gnet.BuiltinEventEngine
+
+	addr                      string
+	atomicNumberOfConnections int64
+
+	bs   *broadcastService
+	pool *goroutine.Pool
+}
+
+type broadcastService struct {
+	connections map[gnet.Conn]struct{}
+}
+
+func (b *broadcastService) broadcastMessage(op ws.OpCode, msg []byte) error {
+	for c, _ := range b.connections {
+		err := wsutil.WriteServerMessage(c, op, msg)
+		if err != nil {
+			return fmt.Errorf("writing server message: %w", err)
+		}
+	}
+	return nil
+}
+
+func (b *broadcastService) trackConnection(c gnet.Conn) {
+	b.connections[c] = struct{}{}
+}
+
+func (b *broadcastService) untrackConnection(c gnet.Conn) {
+	delete(b.connections, c)
+}
+
+type wsCodec struct {
+	upgradedWebsocketConnection bool
+}
+
+func (wss *wsServer) OnBoot(eng gnet.Engine) gnet.Action {
+	logging.Infof("echo server with multi-core=true is listening on %s", wss.addr)
+
+	return gnet.None
+}
+
+func (wss *wsServer) OnOpen(conn gnet.Conn) ([]byte, gnet.Action) {
+	conn.SetContext(new(wsCodec))
+
+	atomic.AddInt64(&wss.atomicNumberOfConnections, 1)
+
+	wss.bs.trackConnection(conn)
+
+	return nil, gnet.None
+}
+
+func (wss *wsServer) OnClose(conn gnet.Conn, err error) gnet.Action {
+	if err != nil {
+		logging.Warnf("error occurred on connection=%s, %v\n", conn.RemoteAddr().String(), err)
+	}
+
+	atomic.AddInt64(&wss.atomicNumberOfConnections, -1)
+	logging.Infof("conn[%v] disconnected", conn.RemoteAddr().String())
+
+	wss.bs.untrackConnection(conn)
+
+	return gnet.None
+}
+
+func (wss *wsServer) OnTraffic(conn gnet.Conn) gnet.Action {
+	codec, ok := conn.Context().(*wsCodec)
+	if !ok {
+		logging.Errorf("unexpected context type, shutting down connection")
+
+		return gnet.Close
+	}
+
+	if !codec.upgradedWebsocketConnection {
+		logging.Infof("conn[%v] upgrade websocket protocol", conn.RemoteAddr().String())
+
+		_, err := ws.Upgrade(conn)
+		if err != nil {
+			logging.Warnf("conn[%v] [err=%v]", conn.RemoteAddr().String(), err.Error())
+
+			return gnet.Close
+		}
+
+		codec.upgradedWebsocketConnection = true
+
+		return gnet.None
+	}
+
+	msg, op, err := wsutil.ReadClientData(conn)
+	if err != nil {
+		if _, ok := err.(wsutil.ClosedError); !ok {
+			logging.Warnf("conn[%v] [err=%v]", conn.RemoteAddr().String(), err.Error())
+		}
+
+		return gnet.Close
+	}
+
+	logging.Infof("conn[%v] receive [op=%v] [msg=%v]", conn.RemoteAddr().String(), op, string(msg))
+
+	err = wss.pool.Submit(func() {
+		wss.bs.broadcastMessage(op, msg)
+	})
+
+	if err != nil {
+		logging.Warnf("conn[%v] [err=%v]", conn.RemoteAddr().String(), err.Error())
+
+		return gnet.Close
+	}
+
+	return gnet.None
+}
+
+func (wss *wsServer) OnTick() (time.Duration, gnet.Action) {
+	logging.Infof("[connected-count=%v]", atomic.LoadInt64(&wss.atomicNumberOfConnections))
+
+	wss.bs.broadcastMessage(ws.OpText, []byte("system: This is a broadcasted system message!"))
+
+	return 3 * time.Second, gnet.None
+}
+
+func main() {
+	var port int
+
+	flag.IntVar(&port, "port", 9000, "server port")
+	flag.Parse()
+
+	pool := goroutine.Default()
+
+	bs := &broadcastService{
+		connections: make(map[gnet.Conn]struct{}),
+	}
+
+	wss := &wsServer{
+		addr: fmt.Sprintf("tcp://0.0.0.0:%d", port),
+		bs:   bs,
+		pool: pool,
+	}
+
+	log.Println(
+		"server exits:",
+		gnet.Run(
+			wss,
+			wss.addr,
+			gnet.WithMulticore(true),
+			gnet.WithReusePort(true),
+			gnet.WithTicker(true),
+		),
+	)
+}
